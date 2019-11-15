bcd565ae9b0e3b146b95f9788968cee549f7c926
Daniel Camacho <2016711@student.uma.pt>
2019-11-15 08:55:12 +0000

    First commit (licenseless)

---
+++ R/main.r
@@ -0,0 +1,12 @@
+#' Visualizing Survival Data
+#' 
+#' This function tries to guess the best format to render the inputted survival data analysis data into a graphically pleasing output, while also explaining any data processing that has occured during the way.
+#' Or it will one day, for now it only goes "Bloop!"
+#' @param data The input data
+#' @export
+#' @examples
+#' whoded()
+
+vsd <- function(data = c()) {
+  print("Bloop!")
+}
